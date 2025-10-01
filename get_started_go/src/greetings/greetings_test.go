7a54e27c0e7c8343a101c3900b65fd14b801c79f
Xiao, Yujiao <yujiao.xiao23@imperial.ac.uk>
2025-10-01 14:24:37 +0100

    Initial commit: Add Go project with greetings module

---
+++ get_started_go/src/greetings/greetings_test.go
@@ -0,0 +1,14 @@
+package greetings_test
+
+import (
+    "testing"
+
+    // "github.com/stretchr/testify/assert"
+
+    "github.com/example/module/src/greetings"
+)
+
+func TestGreeting(t *testing.T) {
+    assert.NotEqual(t, greetings.Greeting(), "")
+}
+

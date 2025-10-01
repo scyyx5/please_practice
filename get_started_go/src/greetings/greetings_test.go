3ac4215d013bc29046a8b62f5d80c17ca6a42d42
scyyx5 <yx223@WhiteLittle.localdomain>
2025-10-01 14:56:22 +0100

    backup code successful for step 8

--- get_started_go/src/greetings/greetings_test.go
+++ get_started_go/src/greetings/greetings_test.go
@@ -2,13 +2,13 @@ package greetings_test
 
 import (
     "testing"
-
-    // "github.com/stretchr/testify/assert"
-
+    
+    // We now need to import the "production" package 
     "github.com/example/module/src/greetings"
 )
 
 func TestGreeting(t *testing.T) {
-    assert.NotEqual(t, greetings.Greeting(), "")
+    if greetings.Greeting() == "" {
+        panic("Greeting failed to produce a result")
+    }
 }
-
